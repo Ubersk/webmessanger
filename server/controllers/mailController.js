d27a60066051727bc0871f88034890c005c6e158
ubersk <kruchinin.s.s@gmail.com>
2022-12-08 16:28:44 +0300

    realization request DB

--- server/controllers/mailController.js
+++ server/controllers/mailController.js
@@ -1,13 +1,48 @@
+const uuid = require("uuid");
+const path = require("path");
+const {Mail} = require("../models/models");
+const ApiError = require("../error/ApiError");
+
 class MailController {
-  async createMsg(req, res) {}
+  async createMsg(req, res, next) {
+    try {
+      const {
+        message_title,
+        message_body,
+        date_create,
+        mail_folderId,
+        userId,
+      } = req.body;
+      const {img} = req.files;
+      let fileName = uuid.v4() + ".jpg";
+      img.mv(path.resolve(__dirname, "..", "static", fileName));
+      const mail = await Mail.create({
+        message_title,
+        message_body,
+        date_create,
+        mail_folderId,
+        userId,
+        img: fileName,
+      });
+      return res.json(mail);
+    } catch (e) {
+      next(ApiError.badRequest(e.message));
+    }
+  }
 
-  async basketMsg(req, res) {}
+  async basketMsg(req, res) {
+  }
 
-  async hideMsg(req, res) {}
+  async hideMsg(req, res) {
+  }
 
-  async getAllMsg(req, res) {}
+  async getAllMsg(req, res) {
+    const mail = await Mail.findAll()
+    return res.json(mail)
+  }
 
-  async getOneMsg(req, res) {}
+  async getOneMsg(req, res) {
+  }
 }
 
 module.exports = new MailController();
--- server/index.js
+++ server/index.js
@@ -5,10 +5,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const models = require("./models/models");
 const cors = require("cors");
+const fileUpload = require("express-fileupload");
 const router = require("./routes/index");
 const errorHandler = require("./middleware/ErrorHandlingMiddleware");
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload({}));
 app.use("/api", router);
 app.use(errorHandler);
 const start = async () => {
