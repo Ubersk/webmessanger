4430a9383039c11c8bd210c46fd44aece0ea1708
ubersk <kruchinin.s.s@gmail.com>
2022-12-08 10:18:34 +0300

    mailController add methods

--- server/controllers/mailController.js
+++ server/controllers/mailController.js
@@ -1,11 +1,29 @@
 class MailController {
   async createMsg(req, res) {}
 
+  async basketMsg(req, res) {}
+
+  async hideMsg(req, res) {}
+
   async deleteMsg(req, res) {}
+
+  async returnBasketMsg(req, res) {}
+
+  async sendMsg(req, res) {}
+
+  async replyMsg(req, res) {}
+
+  async forwardMsg(req, res) {}
 
   async getAllMsg(req, res) {}
 
   async getOneMsg(req, res) {}
+
+  async showContextMenu(req, res) {}
+
+  async showContextMenuMob(req, res) {}
+
+  async showMenuMob(req, res) {}
 }
 
 module.exports = new MailController();
