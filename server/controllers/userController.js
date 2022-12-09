7cc6f3f2258b39c125edcbea311b0cac03a6bff5
ubersk <kruchinin.s.s@gmail.com>
2022-12-09 15:58:14 +0300

    realization authorization

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,14 +1,50 @@
 const ApiError = require("../error/ApiError");
+const bcrypt = require("bcrypt");
+const { User } = require("../models/models");
+const jwt = require("jsonwebtoken");
+
+const generateJwt = (id, name, isAdmin) => {
+  return jwt.sign({ id, name, isAdmin }, process.env.SECRET_KEY, {
+    expiresIn: "24h",
+  });
+};
 
 class UserController {
-  async login(req, res) {}
+  async registration(req, res, next) {
+    const { name, password, isAdmin } = req.body;
+    if (!name || !password) {
+      return next(ApiError.badRequest("Некорректные name или password"));
+    }
+    const candidate = await User.findOne({ where: { name } });
+    if (candidate) {
+      return next(
+        ApiError.badRequest("Пользователь с таким name уже существует!")
+      );
+    }
+    const hashPassword = await bcrypt.hash(password, 5);
+    const user = await User.create({ name, isAdmin, password: hashPassword });
+    const token = generateJwt(user.id, user.name, user.isAdmin);
+    return res.json({ token });
+  }
+
+  async login(req, res, next) {
+    const { name, password } = req.body;
+    const user = await User.findOne({ where: { name } });
+    if (!user) {
+      return next(ApiError.internal("Пользователь не найден"));
+    }
+    let comparePassword = bcrypt.compareSync(password, user.password);
+    if (!comparePassword) {
+      return next(ApiError.internal("Указан неверный пароль"));
+    }
+    const token = generateJwt(user.id, user.name, user.isAdmin);
+    return res.json({ token });
+  }
 
   async check(req, res, next) {
-    const { id } = req.query;
-    if (!id) {
-      return next(ApiError.badRequest("Не задан ID"));
-    }
-    res.json(id);
+    res.json({ message: "All RIGHT!" });
+    // const token = generateJwt(req.user.id, req.user.name, req.user.isAdmin);
+    // return res.json({ token });
   }
 }
 
