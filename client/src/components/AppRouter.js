3fbc742cc5ae3ef73ba68f22e357e612916081a4
kruchinin-ss <kruchinin.s.s@gmail.com>
2022-12-21 08:55:27 +0300

    Use createRoot instead.

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -3,18 +3,21 @@ import React, { useContext } from "react";
 import { Route, Routes } from "react-router-dom";
 import { authRoutes, publicRoutes } from "../routes";
 import { Context } from "../index";
+import AdminMenu from "../pages/AdminMenu";
 
 const AppRouter = () => {
   const { user } = useContext(Context);
   console.log(user);
   return (
     <Routes>
-      {authRoutes.map(({ path, Component }) => (
-        <Route key={path} path={path} element={<Component />} />
-      ))}
+      {user.isAuth &&
+        authRoutes.map(({ path, Component }) => (
+          <Route key={path} path={path} element={<Component />} />
+        ))}
       {publicRoutes.map(({ path, Component }) => (
         <Route key={path} path={path} element={<Component />} />
       ))}
+      <Route path="/admin" element={<AdminMenu />} />
     </Routes>
   );
 };
--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,21 @@
 import React, { createContext } from "react";
-import ReactDOM from 'react-dom';
-import App from './App';
+import ReactDOM from "react-dom";
+import App from "./App";
 import UserStore from "./store/UserStore";
 import MailStore from "./store/MailStore";
+import { createRoot } from "react-dom/client";
 
-export const Context = createContext(null)
-console.log(process.env.REACT_APP_API_URL)
-ReactDOM.render(
-  <Context.Provider value={{
-    user: new UserStore(),
-    mail: new MailStore(),
-  }}>
+export const Context = createContext(null);
+console.log(process.env.REACT_APP_API_URL);
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <Context.Provider
+    value={{
+      user: new UserStore(),
+      mail: new MailStore(),
+    }}
+  >
     <App />
-  </Context.Provider>,
-  document.getElementById('root')
-)
-
+  </Context.Provider>
+);
