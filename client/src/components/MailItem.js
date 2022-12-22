8e9e4554750123e44c093adf2165cffce499f379
ubersk <kruchinin.s.s@gmail.com>
2022-12-22 09:14:18 +0300

    small redesign

--- client/src/components/MailItem.js
+++ client/src/components/MailItem.js
@@ -8,15 +8,14 @@ const MailItem = ({ mail }) => {
   const navigate = useNavigate();
   return (
     <Col
-      md={8}
-      className="d-flex flex-fill mt-3"
+
       onClick={() => navigate(MESSAGE_ROUTES + "/" + mail.id)}
     >
-      <Card style={{ minHeight: 100, cursor: "pointer" }} border={"warning"}>
-        <div className="d-flex justify-content-between  mt-3 ">
-          <div>Автор: {mail.author} |</div>
-          <div>Тема: {mail.name} |</div>
-          <div>{mail.msg_body}|</div>
+      <Card className="mt-3 " style={{ minHeight: 50, cursor: "pointer" }} border={"info"}>
+        <div className="d-flex justify-content-between   mt-3 ">
+          <div className="m-lg-1">Автор: {mail.author}</div>
+          <div  className="m-2 d-inline-block text-truncate">Тема: {mail.name}</div>
+  <div style={{ maxWidth: '150px'}} className="m-2 bg-opacity-75 d-inline-block text-truncate">-{mail.msg_body}</div>
           <div className="d-flex blockquote-footer">{mail.dateCreate}</div>
         </div>
       </Card>
--- client/src/store/MailStore.js
+++ client/src/store/MailStore.js
@@ -3,10 +3,10 @@ import { makeAutoObservable } from "mobx";
 export default class MailStore {
   constructor() {
     this._types = [
-      { id: 1, name: "Удалить" },
-      { id: 2, name: "Создать" },
-      { id: 3, name: "Переслать" },
-      { id: 4, name: "Ответить" },
+      { id: 1, name: "Входящие" },
+      { id: 2, name: "Исходящие" },
+      { id: 3, name: "Спам" },
+      { id: 4, name: "Корзина" },
     ];
     this._funcmail = [
       { id: 1, name: "Удалить" },
@@ -17,21 +17,21 @@ export default class MailStore {
     this._msg = [
       {
         id: 1,
-        author: "Я",
+        author: "Кручинин Святослав Станиславович",
         name: "Празднование Нового года",
         msg_body: "Это пример сообщения.",
         dateCreate: "31.07.2001",
       },
       {
         id: 2,
-        author: "Я",
+        author: "Кручинин Станислав Владимирович",
         name: "Сдача ежегодных отчетов",
-        msg_body: "Это пример сообщения.",
+        msg_body: "Это пример сообщения.Это пример сообщения.Это пример сообщения.Это пример сообщения.Это пример сообщения.Это пример сообщения.",
         dateCreate: "31.07.2001",
       },
       {
         id: 3,
-        author: "Я",
+        author: "Кручинина Владислава Станиславовна",
         name: "Объявление: Котята!",
         msg_body: "ауцпауцйапуцйуйца",
         dateCreate: "31.07.2001",
