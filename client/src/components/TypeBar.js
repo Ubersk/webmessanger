d3018d6d34db9e609ac74a4f824886a729d7afd6
kruchinin-ss <kruchinin.s.s@gmail.com>
2023-05-12 14:47:12 +0300

    filter msg userauth

--- client/src/components/TypeBar.js
+++ client/src/components/TypeBar.js
@@ -5,39 +5,56 @@ import { Context } from "../index";
 const TypeBar = observer(() => {
   const {mailStore} = useContext(Context)
 
-function AllMess(){
+function HowFolder(){
+  const key = 1;
+  switch (key) {
+    case 1:
+      function AllMess(){
+        
+      }
+      
+      break;
 
-}
+    case 2:
+      function NewMess(){
+        
+      }
 
+    break;
 
-function NewMess(){
-  
-}
+    case 3:
+      function SendMess(){
+        
+      }
 
+      break;
 
-function SendMess(){
-  
-}
+    case 4:
+      function TakeMess(){
+        
+      }
 
+    break;
 
-function TakeMess(){
-  
-}
-
-
-function DelMess(){
-  
+    case 5:
+      function DelMess(){
+       
+      }
+    break;
+    default:
+      break;
+  }
 }
 
   return (
     <div id="typebar" className="container">
       <div className="col">
         <div className="d-flex flex-column">
-          <button type="button" className="btn btn-outline-dark btn-lg ms-1" onClick={NewMess()}>Все сообщения</button>
-          <button type="button" className="btn btn-outline-primary ms-1 mt-4">Новые</button>
-          <button type="button" className="btn btn-outline-primary ms-1 mt-1">Отправленные</button>
-          <button type="button" className="btn btn-outline-primary ms-1 mt-1">Полученные</button>
-          <button type="button" className="btn btn-outline-danger ms-1 mt-3 mb-2">Корзина</button>
+          <button type="button" className="btn btn-outline-dark btn-lg ms-1" onClick={HowFolder(1)}>Все сообщения</button>
+          <button type="button" className="btn btn-outline-primary ms-1 mt-4" onClick={HowFolder(2)}>Новые</button>
+          <button type="button" className="btn btn-outline-primary ms-1 mt-1" onClick={HowFolder(3)}>Отправленные</button>
+          <button type="button" className="btn btn-outline-primary ms-1 mt-1" onClick={HowFolder(4)}>Полученные</button>
+          <button type="button" className="btn btn-outline-danger ms-1 mt-3 mb-2" onClick={HowFolder(5)}>Корзина</button>
         </div>
       </div>
 
