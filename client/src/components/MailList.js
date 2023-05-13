843634a8eab179968a6672596a81fc613754c187
ubersk <kruchinin.s.s@gmail.com>
2023-05-13 19:50:49 +0300

    search complete

--- client/src/components/MailList.js
+++ client/src/components/MailList.js
@@ -4,20 +4,22 @@ import { Context } from "../index";
 import {  Row } from "react-bootstrap";
 import MailItem from "./MailItem";
 
+
+
 const MailList = observer(() => {
   const {mailStore, userStore} = useContext(Context)
   // console.log("Пользователь:", userStore.user.name,"Авторизован:", userStore.isAuth)
   const idUserAuth = userStore.user.id;
-  console.log(idUserAuth);
   let msgs = mailStore.msg.filter(item => item.userIdUser === idUserAuth || item.user_creator === idUserAuth);
-  
+  let filter = mailStore.search;
   console.log(msgs);
-  
-  if (mailStore.search !==[]){
-  msgs = mailStore.msg.filter(item => item.message_title == mailStore.search || item.message_body == mailStore.search);
-  mailStore.setSearch([]);
-  }
-  else{
+
+  if (filter !== "") {
+    msgs = msgs.filter(item => item.message_body.includes(filter) || item.message_title.includes(filter));
+    console.log(filter);
+    console.log(msgs);
+  } else {
+    msgs = mailStore.msg.filter(item => item.userIdUser === idUserAuth || item.user_creator === idUserAuth);
   }
 
   return (
